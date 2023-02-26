10639812396f7ac21d1883090802e53b582b589e
JosePhee03 <tyttocr@gmail.com>
2023-02-26 20:17:52 -0300

    add theme extends

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,jsx}"],
+  content: ['./src/**/*.{html,js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'pastel-blue': '#0095FF',
+        'pastel-light-blue': '#3AB0FF',
+        'pastel-gray': '#EEEEEE',
+        'pastel-cyan': '#30E3DF',
+        'pastel-dark-cyan': '#30BEBB'
+      },
+      backgroundImage: {
+        alpha: 'url(/public/alpha.jpg)'
+      },
+      fontFamily: {
+        fredoka: 'Fredoka, sans-serif'
+      },
+      dropShadow: {
+        'text-shadow': '0 0 4px 4px #00000'
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
