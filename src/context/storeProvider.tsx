b64d3cbadbcf3acce97794d0464e9ced8dac4bf8
JosePhee03 <tyttocr@gmail.com>
2023-03-03 11:45:55 -0300

    Create StoreProvider

---
+++ src/context/storeProvider.tsx
@@ -0,0 +1,28 @@
+import React, { useContext, useReducer } from 'react'
+import { imageContext } from '../types/ImagStatusType'
+import { StoreContext } from './storeContext'
+import { storeReducer } from './storeReducer'
+
+const initialStore: imageContext = {
+  imageState: null,
+  originalImage: null,
+  editedImage: null,
+  fileImage: null,
+}
+
+interface props {
+  children: JSX.Element | JSX.Element[]
+}
+
+
+function StoreProvider ({ children }:props) {
+  const [store, dispatch] = useReducer(storeReducer, initialStore)
+
+  return (
+    <StoreContext.Provider value={{store, dispatch}}>
+      {children}
+    </StoreContext.Provider>
+  )
+}
+
+export {StoreProvider, StoreContext}
