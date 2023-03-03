a55fddd74ceed8503da02487eaf69d2609df5d5a
JosePhee03 <tyttocr@gmail.com>
2023-03-03 11:40:17 -0300

    create Description component

---
+++ src/components/Desciption.jsx
@@ -0,0 +1,9 @@
+export function Description () {
+  return (
+    <>
+      <h2 className='font-bold font-fredoka text-2xl text-pastel-blue'>De imagen a sticker</h2>
+      <h4 className='font-fredoka'>Convierte cualquier imagen en un sticker de forma rapida y sencilla<br/> con esta herramienta.</h4>
+    </>
+
+  )
+}
