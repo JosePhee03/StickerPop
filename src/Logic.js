c05259e5572a894fbec31902f24a4fefa7da62c0
JosePhee03 <tyttocr@gmail.com>
2023-03-03 11:46:12 -0300

    Delete logic.js

--- src/Logic.js
+++
@@ -1,8 +0,0 @@
-export function ImageURL (file, setImagePreview) {
-  const fileRender = new FileReader()
-  fileRender.readAsDataURL(file)
-  fileRender.addEventListener('load', () => {
-    const fileURL = fileRender.result
-    setImagePreview(fileURL)
-  })
-}
