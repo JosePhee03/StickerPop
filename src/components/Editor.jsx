d319d4f0731edf9d7216a5e63b67e1ff4a26cef2
JosePhee03 <tyttocr@gmail.com>
2023-03-06 20:19:06 -0300

    fixed editor image

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -2,26 +2,22 @@ import { useContext, useState } from 'react'
 import { StoreContext } from '../context/storeContext'
 import { ButtonMenu } from './ButtonMenu'
 import { EditedImage } from './EditedImage'
-import { ModalEditor } from './ModalEditor'
 
 export function Editor () {
   const { store } = useContext(StoreContext)
-  const { editedImage, ObjectOfEditedImage } = store
+  const { editedImage } = store
   const [modalEditor, setModalEditor] = useState(false)
 
   return (
     <>
-      {modalEditor ? <ModalEditor setModalEditor={setModalEditor}/> : '' }
       <div className="z-0 h-auto flex flex-col gap-2 m-auto">
-        <ButtonMenu setModalEditor={setModalEditor}/>
-        <div className='bg-blue-50 rounded-lg w-80 p-5 '>
-          <div className='relative w-full h-full flex justify-center items-center'>
+        <ButtonMenu/>
+        <div className='bg-blue-50 w-80 h-80 rounded-lg flex justify-center items-center'>
             <EditedImage/>
-          </div>
-
         </div>
           <a
-            download={editedImage}
+            download
+            href={editedImage}
             className='btn w-full bg-cyan-400 border-cyan-500 cursor-pointer flex justify-center items-center'
             title='Descargar imagen'
           >
