b7975732b970260087ccfc8c8e43bbf4c8cc6975
JosePhee03 <tyttocr@gmail.com>
2023-03-06 20:20:55 -0300

    fixed outline effect

--- src/service/CloudEffect.ts
+++ src/service/CloudEffect.ts
@@ -1,11 +1,9 @@
 import { outline } from '@cloudinary/url-gen/actions/effect'
 import { trim } from '@cloudinary/url-gen/actions/reshape'
 import { CLOUD_CONFIG } from './CloudConfig'
-import { scale } from '@cloudinary/url-gen/actions/resize'
-import { CloudinaryImage } from '@cloudinary/url-gen'
 
 export function CloudEffect (publiId:string, width:number) {
-  const borderSize = Math.floor(width / 64)
+  const borderSize = Math.floor(width / 32)
 
   const cloundImage = CLOUD_CONFIG.image(publiId)
     .effect(outline()
@@ -14,13 +12,7 @@ export function CloudEffect (publiId:string, width:number) {
       .color('white')
     )
     .reshape(trim())
-  // .backgroundColor('')
+  
   const imageEditedURL = cloundImage.toURL()
   return [imageEditedURL, cloundImage]
-}
-
-export function cloudBackgroudEffect (cloundImage: CloudinaryImage, color: string) {
-  
-  const changeBackgroundColor = cloundImage.backgroundColor(color)
-  return changeBackgroundColor
 }
