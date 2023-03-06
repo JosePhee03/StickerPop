9670e7cafdf17827a140b3df3636c2126b212c4c
JosePhee03 <tyttocr@gmail.com>
2023-03-06 20:17:37 -0300

    add progress bar animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -12,13 +12,27 @@ module.exports = {
       },
       backgroundImage: {
         alpha: 'url(/public/alpha.jpg)',
-        'footer-gradient': 'linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(0, 0, 0, 0.1) 100%);'
+        'footer-gradient': 'linear-gradient(180deg, rgba(217, 217, 217, 0) 0%, rgba(0, 0, 0, 0.1) 100%)',
+        'bar-gradient': 'repeating-linear-gradient(45deg, transparent, transparent 2.5rem, white 2.5rem, white 5rem)'
       },
       fontFamily: {
         fredoka: 'Fredoka, sans-serif'
       },
       dropShadow: {
         'text-shadow': '0 0 4px 4px #00000'
+      },
+      animation: {
+        'progress-bar': 'slide 4s linear infinite'
+      },
+      keyframes: {
+        slide: {
+          from: {
+            'background-position-x': '0'
+          },
+          to: {
+            'background-position-x': '113px'
+          }
+        }
       }
     }
   },
