1ad5b61885bd98bcf9acbfeff1545e65ec6416bc
JosePhee03 <tyttocr@gmail.com>
2023-03-06 15:31:08 -0300

    refactor storereducer

--- src/context/storeReducer.ts
+++ src/context/storeReducer.ts
@@ -1,14 +1,14 @@
 import {fileType, imageContext, imageStatus} from '../types/ImagStatusType'
 
 export type imageAction = 
-  | {type: imageStatus, payload: string }
-  | {type: 'DROP', payload: [string, fileType] }
+  | {type: imageStatus, payload: any }
 
 
 function storeReducer (state: imageContext, action:imageAction):imageContext {
   switch (action.type) {
     case 'RESET':
       return {
+        ...state,
         imageState: null,
         editedImage: null,
         originalImage: null,
@@ -16,17 +16,22 @@ function storeReducer (state: imageContext, action:imageAction):imageContext {
       }
     case 'DROP':
       return {
+        ...state,
         imageState: action.type,
-        editedImage: state.editedImage,
         originalImage: action.payload[0],
         fileImage: action.payload[1]
       }
-      return {...state, imageState: action.type}
     case 'EDITED':
       return {
         ...state,
+        ObjectOfEditedImage: action.payload
+      }
+    case 'UPLOAD':
+      return {
+        ...state,
         imageState: action.type,
-        editedImage: action.payload
+        editedImage: action.payload[0],
+        fileImage: action.payload[1]
       }
     default:
       return state
