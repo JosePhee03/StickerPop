bd2a73f70b7d617c2224078a7473de3b178dff80
JosePhee03 <tyttocr@gmail.com>
2023-03-06 20:18:30 -0300

    delete editor modal

--- src/components/ButtonMenu.jsx
+++ src/components/ButtonMenu.jsx
@@ -2,21 +2,16 @@ import { useContext } from 'react'
 import { StoreContext } from '../context/storeContext'
 import { Button } from './Button'
 
-export function ButtonMenu ({ setModalEditor }) {
+export function ButtonMenu () {
   const { dispatch } = useContext(StoreContext)
 
   const handleBack = () => {
     dispatch({ type: 'RESET' })
   }
 
-  const handleEdit = () => {
-    setModalEditor(true)
-  }
-
   return (
     <div className='flex justify-between'>
       <Button onClick={handleBack} className={'bg-orange-400 border-orange-500'} >Volver Atrás</Button>
-      <Button onClick={handleEdit} className={'bg-green-400 border-green-500'}>Editar Imagen</Button>
     </div>
   )
 }
