3658563ad5b1c87c41291975394cc102f6c078b5
JosePhee03 <tyttocr@gmail.com>
2023-03-11 18:10:31 -0300

    style responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export function Card ({ children }) {
   return (
-    <section className='flex flex-col z-10 gap-4 mt-24 py-10 px-8 rounded-lg bg-white shadow-lg shadow-gray-200'>
+    <section className='flex flex-col z-10 gap-4 sm:mt-24 py-10 px-8 rounded-lg bg-white sm:shadow-lg shadow-gray-200'>
       {children}
     </section>
   )
