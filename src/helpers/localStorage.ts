b10d42f007bdf283187fa8651e5184036440c0af
JosePhee03 <tyttocr@gmail.com>
2023-03-11 18:35:22 -0300

    create new helper localStorage

---
+++ src/helpers/localStorage.ts
@@ -0,0 +1,14 @@
+import { imageContext } from "../types/ImagStatusType"
+
+type keyType = 'GLOBAL_STATE' | 'EFFECT_STATE'
+
+export function setlocalStorage (key:keyType, value:imageContext | boolean) {
+  const valueJSON = JSON.stringify(value)
+  return window.localStorage.setItem(key, valueJSON)
+}
+
+export function getLocalStorage (key:keyType):imageContext | null {
+  const getStorage = window.localStorage.getItem(key)
+  return getStorage ? JSON.parse(getStorage) : null 
+}
+
