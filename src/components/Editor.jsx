3bc2113671d0a3d3f4bfbd8c6f0ef9664180afe9
JosePhee03 <tyttocr@gmail.com>
2023-03-11 18:12:05 -0300

    add components and styles

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,28 +1,35 @@
 import { useContext, useState } from 'react'
 import { StoreContext } from '../context/storeContext'
+import { downloadImage } from '../service/DownloadImage'
+import { Button } from './Button'
 import { ButtonMenu } from './ButtonMenu'
 import { EditedImage } from './EditedImage'
 
 export function Editor () {
   const { store } = useContext(StoreContext)
   const { editedImage } = store
-  const [modalEditor, setModalEditor] = useState(false)
+  const [processingImage, setProcessingImage] = useState(true)
+
+  const handleDownload = async () => {
+    await downloadImage(editedImage, 'mi-sticker.png')
+  }
 
   return (
     <>
       <div className="z-0 h-auto flex flex-col gap-2 m-auto">
-        <ButtonMenu/>
+        <ButtonMenu setProcessingImage={setProcessingImage}/>
         <div className='bg-blue-50 w-80 h-80 rounded-lg flex justify-center items-center'>
-            <EditedImage/>
+            <EditedImage processingImage={processingImage} setProcessingImage={setProcessingImage}/>
         </div>
+        <Button onClick={handleDownload} className='bg-sky-400 border-sky-500'>
           <a
+            className='w-full h-full'
             download
-            href={editedImage}
-            className='btn w-full bg-cyan-400 border-cyan-500 cursor-pointer flex justify-center items-center'
             title='Descargar imagen'
           >
             Descargar Sticker
           </a>
+        </Button>
       </div>
     </>
   )
