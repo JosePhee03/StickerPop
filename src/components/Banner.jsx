332cf45adbbc45af438becdd5c82a7d9ce37eeca
JosePhee03 <tyttocr@gmail.com>
2023-03-11 18:09:00 -0300

    styles modifield

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -2,13 +2,13 @@ import { StickerPopLogo, Wave } from '../icons'
 
 export function Banner () {
   return (
-    <header className='absolute top-0 left-0 w-full -z-10'>
+    <header className='relative sm:absolute top-0 left-0 w-full -z-10'>
       <Wave/>
       <span className='absolute top-4 left-4'>
       <StickerPopLogo/>
       </span>
-      <picture className='absolute top-4 right-0 hidden lg:block'>
-        <img className='w-40' src="/globo.png" alt="globo" />
+      <picture className='absolute top-4 right-4 hidden lg:block'>
+        <img className='w-28' src="/globo.png" alt="globo" />
       </picture>
     </header>
   )
