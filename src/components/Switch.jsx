1ef5ef15ca24a9b1ce08cabb9be0cc5b7d0b46b4
JosePhee03 <tyttocr@gmail.com>
2023-03-11 18:14:55 -0300

    add save change in local storage

--- src/components/Switch.jsx
+++ src/components/Switch.jsx
@@ -1,11 +1,16 @@
-import { useContext } from 'react'
+import { useContext, useEffect } from 'react'
 import { StoreContext } from '../context/storeContext'
+import { setlocalStorage } from '../helpers/localStorage'
 import { Editor } from './Editor'
 import { Form } from './Form'
 
 export function Switch () {
   const { store } = useContext(StoreContext)
   const { imageState } = store
+
+  useEffect(() => {
+    setlocalStorage('GLOBAL_STATE', store)
+  }, [store])
 
   switch (imageState) {
     case 'UPLOAD':
